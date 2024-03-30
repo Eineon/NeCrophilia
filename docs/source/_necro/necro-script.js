4912b109175d26185bc9e05cb972b5aa992ac34b
Eineon <asfrtrajio@gmail.com>
2024-03-30 11:27:21 +0800

    up

--- docs/source/_necro/necro-script.js
+++ docs/source/_necro/necro-script.js
@@ -1,10 +1,11 @@
 window.onload = function() {
-  var 绝技 = document.getElementById('绝技');
-  for (var i = 0; i < 绝技.length; i++) {
-    绝技[i].innerText = "每一刻最多只能宣言1次 [绝技] 动作";
+  var elements = document.getElementsByClassName('show');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    if (element.id === '绝技') {
+      element.innerText = "每一刻最多只能宣言1次 [绝技] 动作";
+    } else if (element.id === '起手') {
+      element.innerText = "只能在尚未宣言过 [移动]、[攻击] 或 [起手] 动作的刻度中使用";
+    }
   }
-  var 起手 = document.getElementById('起手');
-  for (var i = 0; i < 起手.length; i++) {
-    起手[i].innerText = "只能在尚未宣言过 [移动]、[攻击] 或 [起手] 动作的刻度中使用";
-  }
-}; 
+};
